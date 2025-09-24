7db27cfac4645d8b2eed9aff6749594a33ddceca
Kimutaijeremy <jrmkimutai@gmail.com>
2025-09-24 20:14:23 +0300

    using fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    // Append the id to the DOM
+    document.body.innerHTML = object.id;
+  })
+  .catch(function(error) {
+    // Append error message to the DOM
+    document.body.innerHTML = error.message;
+  });
+}
